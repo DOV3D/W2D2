22970e4960f7d6232ade2649694a2dcfbfc278a3
DOV3D <davidovelazquez3@gmail.com>
2021-08-17 09:25:58 -0400

    <guessing game project>

---
+++ david_velazquez_guessing_game_project/lib/guessing_game.rb
@@ -0,0 +1,19 @@
+class GuessingGame
+    
+    def initialize(min, max)
+        @min = min
+        @max = max
+        @secret_num = rand(min..max)
+        @num_attempts = 0
+        @game_over = false
+    end
+
+    def num_attempts
+        @num_attempts
+    end
+
+
+
+
+
+end #End class
