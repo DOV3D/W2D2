821cd66b00f56bc143ed0da6036313243e5183fa
DOV3D <davidovelazquez3@gmail.com>
2021-08-17 17:12:04 -0400

    <completed startup project>

--- david_velazquez_startup_project/lib/employee.rb
+++ david_velazquez_startup_project/lib/employee.rb
@@ -1,3 +1,19 @@
 class Employee
+    def initialize(name, title)
+        @name = name
+        @title = title
+        @earnings = 0
+    end
+    def name
+        @name
+    end
 
-end
+    def title
+        @title
+    end
+
+    def pay(num)
+        @earnings += num
+    end
+    
+end #Class End
--- david_velazquez_startup_project/lib/startup.rb
+++ david_velazquez_startup_project/lib/startup.rb
@@ -1,5 +1,84 @@
 require "employee"
 
 class Startup
+    def initialize(name, funding, salaries)
+        @name = name                #String
+        @funding = funding          #num
+        @salaries = salaries        #hash containing 'title` => `salary` pairs
+        @employees = []
+    end
 
-end
+    def name
+        @name
+    end
+
+    def funding
+        @funding
+    end
+
+    def salaries
+        @salaries
+    end
+
+    def employees
+        @employees
+    end
+
+    def valid_title?(title)
+        @salaries.has_key?(title)
+    end
+
+    def >(startup)
+        self.funding > startup.funding
+    end
+
+    def hire(emp_name, title)
+        if valid_title?(title)
+           @employees << Employee.new(emp_name, title)
+        else
+         raise "Error"   
+        end
+    end
+
+    def size
+        @employees.length
+    end
+
+    def pay_employee(emp_instance)
+        if @funding > @salaries[emp_instance.title]
+            emp_instance.pay(@salaries[emp_instance.title])
+            @funding -= @salaries[emp_instance.title]
+        else
+            raise "Error"
+        end
+    end
+
+    def payday
+        @employees.each {|employee| pay_employee(employee) }
+    end
+
+    def average_salary
+        sum = 0
+        @employees.each do |employee|
+           sum += @salaries[employee.title]
+        end
+        sum / @employees.length
+    end
+
+    def close
+        @employees = []
+        @funding = 0
+    end
+
+    def acquire(startup)
+        @funding += startup.funding
+        startup.salaries.each do |title, salary|
+            if !@salaries.has_key?(title)
+                @salaries[title] = salary
+            end
+        end
+        @employees += startup.employees
+        startup.close
+    end
+
+end #Class End
